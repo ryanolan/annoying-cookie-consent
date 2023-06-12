c6ea387c8501c1fceee972524d069787c9e1498c
rywilldev <rywilldav@gmail.com>
2023-06-12 13:34:42 -0400

    update script.js
    added innerHTML to apply loading effect when user clicks accept

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -7,4 +9,14 @@ setTimeout(function () {
 
 modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
+});
+
+consentForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  modalText.innerHTML = `<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>`;
 });
