e1aed4953fcf01eff6a32fc2b5088bff64969b63
rywilldev <rywilldav@gmail.com>
2023-06-12 13:49:24 -0400

    update script.js
    added two setTimeout methods once user clicks "Accept" button

--- script.js
+++ script.js
@@ -19,4 +19,17 @@ consentForm.addEventListener("submit", function (event) {
         Uploading your data to the dark web...
     </p>
 </div>`;
+  setTimeout(function () {
+    document.getElementById("uploadText").innerText = `Making the sale...`;
+  }, 1500);
+
+  setTimeout(function () {
+    document.getElementById(
+      "modal-inner"
+    ).innerHTML = `<h2>Thanks you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>`;
+  }, 3000);
 });
