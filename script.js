a7378310928912e96d41ea1c7fe3d49ebf1e7b81
rywilldev <rywilldav@gmail.com>
2023-06-12 15:28:07 -0400

    update index.html, script.js
    add disabled to close button until the user reaches the third modal message (pirate gif)

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ consentForm.addEventListener("submit", function (event) {
   const consentFormData = new FormData(consentForm);
   const fullName = consentFormData.get("modalName");
 
-
   modalText.innerHTML = `<div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
     <p id="uploadText">
@@ -36,5 +35,6 @@ consentForm.addEventListener("submit", function (event) {
         <div class="idiot-gif">
             <img src="images/pirate.gif">
         </div>`;
+    modalCloseBtn.disabled = false;
   }, 3000);
 });
